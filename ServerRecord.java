42356aeae7e93b9721f78c14106884a3dd37bcf2
borg668 <fred.ogrady@gmail.com>
2015-11-16 11:13:09 -0500

    Updated README File to add better Discription

---
+++ ServerRecord.java
@@ -0,0 +1,93 @@
+/*
+ * ServerRecord.java
+ *
+ * Created on 17 March 2004, 11:43
+ */
+
+package distrit.server;
+
+import distrit.InteractiveTaskServer;
+
+import java.util.HashSet;
+
+/** Record representing an InteractiveTaskServer.  Used for managing the
+ * allocation of multiple clients to multiple servers in a multi server
+ * environment.
+ * @author mmg20
+ */
+public class ServerRecord {
+    
+    protected String name;
+    protected InteractiveTaskServer server;
+    protected String specificClientName;
+    protected HashSet allocatedClients = new HashSet();
+    
+    /** Creates a new instance of ServerRecord
+     * @param name A unique name for this server
+     * @param server Actual InteractiveTaskServer object
+     * @param specificClientName Name of client specifically allocated to this server.  A ""
+     * or null value define a server with no specific client
+     */
+    public ServerRecord( String name, InteractiveTaskServer server, String specificClientName ) {
+        this.name = name;
+        this.server = server;
+        this.specificClientName = specificClientName;
+    }
+    
+    /** Get method for this server's unique name
+     * @return This server's unique name
+     */    
+    public String getName() {
+        return name;
+    }
+    
+    /** Get method for the actual server object within this record
+     * @return The actual server object within this record
+     */    
+    public InteractiveTaskServer getServer() {
+        return server;
+    }
+    
+    /** Get method for the name of the client specifically allocated to this server
+     * @return The name of the client specifically allocated to this server
+     */    
+    public String getSpecificClientName() {
+        return specificClientName;
+    }
+    
+    /** Set method for the name of the client specifically allocated to this server
+     * @param specificClientName The name of the client specifically allocated to this server
+     */
+    public void setSpecificClientName( String specificClientName )
+    {
+        this.specificClientName = specificClientName;
+    }
+    
+    /** Adds a client to the set allocated to this server
+     * @param cr Record of client to be added to the allocation set
+     */    
+    public void allocateClient(ClientRecord cr) {
+        allocatedClients.add( cr );
+    }
+    
+    /** Removes a client to the set allocated to this server
+     * @param cr Record of client to be removed to the allocation set
+     */    
+    public void deallocateClient(ClientRecord cr) {
+        allocatedClients.remove( cr );
+    }
+    
+    /** Get method for the set of clients currently allocated to this server
+     * @return The set of clients currently allocated to this server
+     */
+    public HashSet getAllocatedClients()
+    {
+        return allocatedClients;
+    }
+    
+    public String toString()
+    {
+        String rv = name;
+        return rv;
+    }
+}
